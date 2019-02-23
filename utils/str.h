6a49a91a7b44cc4735663467f613bb1e2f3217f9
Emerson MX <emersonmx@gmail.com>
2019-02-22 22:26:13 -0300

    Add str.h

---
+++ utils/str.h
@@ -0,0 +1,32 @@
+#ifndef UTILS_STR_H
+#define UTILS_STR_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char* str_format(const char* format, ...)
+{
+    va_list args1;
+    va_start(args1, format);
+    va_list args2;
+    va_copy(args2, args1);
+    int size = vsnprintf(NULL, 0, format, args1);
+    va_end(args1);
+    int total_size = size + 1;
+    char* buffer = calloc(total_size, sizeof(char));
+    vsnprintf(buffer, total_size, format, args2);
+    va_end(args2);
+
+    return buffer;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UTILS_STR_H */
