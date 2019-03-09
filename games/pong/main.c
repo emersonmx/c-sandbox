1e18e902a2a3e4bd26de3c9ccf3bd4b8ea0b5a26
Emerson MX <emersonmx@gmail.com>
2019-03-08 21:31:23 -0300

    Refactor pong code

--- games/pong/main.c
+++ games/pong/main.c
@@ -45,13 +45,16 @@ typedef struct Game {
 } Game;
 
 
-Game game;
-
-void initialize_settings(void);
-void setup_game(void);
+static Game game;
 
 void initialize_game(void);
+void initialize_settings(void);
+bool setup_window(void);
+bool setup_renderer(void);
+void setup_game(void);
 void finalize_game(void);
+void destroy_renderer(void);
+void destroy_window(void);
 
 void renderer_clear(void);
 void renderer_draw_rect(SDL_Rect rect, SDL_Color color);
@@ -59,13 +62,15 @@ void renderer_present(void);
 
 void player_render(Player* player);
 
+int window_width(void);
+int window_height(void);
+SDL_Color clear_color(void);
+
 int main(void)
 {
-    initialize_settings();
+    atexit(finalize_game);
 
     initialize_game();
-
-    setup_game();
 
     while (game.running) {
         SDL_Event event;
@@ -81,9 +86,18 @@ int main(void)
         renderer_present();
     }
 
-    finalize_game();
-
     return 0;
+}
+
+void initialize_game(void)
+{
+    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_INFO);
+
+    initialize_settings();
+
+    game.running = setup_window() && setup_renderer();
+
+    setup_game();
 }
 
 void initialize_settings(void)
@@ -103,9 +117,43 @@ void initialize_settings(void)
     };
 }
 
+bool setup_window(void)
+{
+    game.window = SDL_CreateWindow(
+        game.settings.window.title,
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        game.settings.window.width, game.settings.window.height,
+        SDL_WINDOW_SHOWN
+    );
+    if (game.window == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_VIDEO,
+            "Couldn't create SDL Window\n\tError: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+bool setup_renderer(void)
+{
+    Uint32 flags = SDL_RENDERER_ACCELERATED;
+    if (game.settings.renderer.vsync) {
+        flags |= SDL_RENDERER_PRESENTVSYNC;
+    }
+
+    game.renderer = SDL_CreateRenderer(game.window, -1, flags);
+    if (game.renderer == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_VIDEO,
+            "Couldn't create SDL Renderer\n\tError: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
 void setup_game(void)
 {
-    float center_y = game.settings.window.height/2.0f;
+    float center_y = window_height()/2.0f;
     game.players[PLAYER1] = (Player){
         .color = {255, 255, 255, SDL_ALPHA_OPAQUE},
         .shape = {0, 0, 20, 80},
@@ -114,80 +162,31 @@ void setup_game(void)
     game.players[PLAYER2] = (Player){
         .color = {255, 255, 255, SDL_ALPHA_OPAQUE},
         .shape = {200, 0, 20, 80},
-        .position = {game.settings.window.width - 15, center_y, 0}
+        .position = {window_width() - 15, center_y, 0}
     };
 }
 
-SDL_Window* create_sdl_window(void)
+void finalize_game(void)
 {
-    SDL_Window* window = SDL_CreateWindow(
-        game.settings.window.title,
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        game.settings.window.width, game.settings.window.height,
-        SDL_WINDOW_SHOWN
-    );
-    RETURN_NULL_IF_NULL(window);
-
-    return window;
+    destroy_renderer();
+    destroy_window();
 }
 
-void destroy_sdl_window(void)
-{
-    SDL_DestroyWindow(game.window);
-    game.window = NULL;
-}
-
-SDL_Renderer* create_sdl_renderer(void)
-{
-    Uint32 flags = SDL_RENDERER_ACCELERATED;
-    if (game.settings.renderer.vsync) {
-        flags |= SDL_RENDERER_PRESENTVSYNC;
-    }
-
-    SDL_Renderer* renderer = SDL_CreateRenderer(game.window, -1, flags);
-    RETURN_NULL_IF_NULL(renderer);
-
-    return renderer;
-}
-
-void destroy_sdl_renderer(void)
+void destroy_renderer(void)
 {
     SDL_DestroyRenderer(game.renderer);
     game.renderer = NULL;
 }
 
-void initialize_game(void)
+void destroy_window(void)
 {
-    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_INFO);
-
-    game.running = false;
-
-    game.window = create_sdl_window();
-    if (game.window == NULL) {
-        SDL_LogError(SDL_LOG_CATEGORY_VIDEO,
-            "Couldn't create SDL Window\n\tError: %s\n", SDL_GetError());
-        return;
-    }
-
-    game.renderer = create_sdl_renderer();
-    if (game.renderer == NULL) {
-        SDL_LogError(SDL_LOG_CATEGORY_VIDEO,
-            "Couldn't create SDL Renderer\n\tError: %s\n", SDL_GetError());
-        return;
-    }
-
-    game.running = true;
-}
-
-void finalize_game(void)
-{
-    destroy_sdl_renderer();
-    destroy_sdl_window();
+    SDL_DestroyWindow(game.window);
+    game.window = NULL;
 }
 
 void renderer_clear(void)
 {
-    SDL_Color color = game.settings.renderer.clear_color;
+    SDL_Color color = clear_color();
     SDL_SetRenderDrawColor(game.renderer, color.r, color.g, color.b, color.a);
     SDL_RenderClear(game.renderer);
 }
@@ -210,4 +209,19 @@ void player_render(Player* player)
     rect.y = player->position[1] - rect.h/2.0f;
     SDL_Color color = player->color;
     renderer_draw_rect(rect, color);
+}
+
+int window_width(void)
+{
+    return game.settings.window.width;
+}
+
+int window_height(void)
+{
+    return game.settings.window.height;
+}
+
+SDL_Color clear_color(void)
+{
+    return game.settings.renderer.clear_color;
 }
