82793e4887182508c125a7ac89459d970a315bde
Emerson MX <emersonmx@gmail.com>
2019-03-09 16:26:54 -0300

    Create basic sdl2 engine

---
+++ sdl2/engine.c
@@ -0,0 +1,265 @@
+#include "engine.h"
+
+#include <sdl2/timer.h>
+#include <utils/macros.h>
+
+void default_init_func(void) {}
+void default_quit_func(void) {}
+void default_process_events_func(SDL_Event* event) {
+    printf("PROCESS_EVENTS\n");
+}
+void default_fixed_update_func(double delta) {}
+void default_update_func(double delta) {}
+void default_render_func(void) {}
+
+struct Engine {
+    EngineSettings settings;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+    bool running;
+    int error_code;
+    double physics_tick_count;
+
+    EngineInitFunc init_func;
+    EngineQuitFunc quit_func;
+    EngineProcessEvents process_events_func;
+    EngineFixedUpdateFunc fixed_update_func;
+    EngineUpdateFunc update_func;
+    EngineRenderFunc render_func;
+};
+
+static const EngineSettings default_settings = {
+    .window = {
+        "engine.Window",
+        800, 600
+    },
+    .renderer = {
+        .clear_color = {0, 0, 0, SDL_ALPHA_OPAQUE},
+        .vsync = true
+    },
+    .physics = {
+        .fps = 60.0
+    }
+};
+
+static Engine engine = {
+    .settings = default_settings,
+    .window = NULL,
+    .renderer = NULL,
+
+    .running = true,
+    .error_code = 0,
+    .physics_tick_count = 0.0,
+
+    .init_func = default_init_func,
+    .quit_func = default_quit_func,
+    .process_events_func = default_process_events_func,
+    .fixed_update_func = default_fixed_update_func,
+    .update_func = default_update_func,
+    .render_func = default_render_func,
+};
+
+static double fixed_update_delta(void);
+static void initialize_engine(void);
+static bool setup_window(void);
+static bool setup_renderer(void);
+static void finalize_engine(void);
+static void destroy_renderer(void);
+static void destroy_window(void);
+static void process_events(void);
+static void fixed_update(void);
+static void update(double delta);
+static void render(void);
+
+int engine_main_loop(EngineSettings settings)
+{
+    engine.settings = settings;
+
+    atexit(finalize_engine);
+
+    initialize_engine();
+
+    double last_count = timer_get_ticks_in_seconds();
+    double delta = fixed_update_delta();
+
+    while (engine.running) {
+        double now = timer_get_ticks_in_seconds();
+        delta = now - last_count;
+        last_count = now;
+        engine.physics_tick_count += delta;
+
+        process_events();
+        fixed_update();
+        update(delta);
+        render();
+    }
+
+    return engine.error_code;
+}
+
+void engine_quit_loop(void)
+{
+    engine.running = false;
+}
+
+EngineSettings engine_default_settings(void)
+{
+    return default_settings;
+}
+
+EngineSettings engine_settings(void)
+{
+    return engine.settings;
+}
+
+SDL_Window* engine_window(void)
+{
+    return engine.window;
+}
+
+SDL_Renderer* engine_renderer(void)
+{
+    return engine.renderer;
+}
+
+void engine_set_init_func(EngineInitFunc func)
+{
+    ASSERT_VALID_OBJECT(func);
+    engine.init_func = func;
+}
+
+void engine_set_quit_func(EngineQuitFunc func)
+{
+    ASSERT_VALID_OBJECT(func);
+    engine.quit_func = func;
+}
+
+void engine_set_process_events_func(EngineProcessEvents func)
+{
+    ASSERT_VALID_OBJECT(func);
+    engine.process_events_func = func;
+}
+
+void engine_set_fixed_update_func(EngineFixedUpdateFunc func)
+{
+    ASSERT_VALID_OBJECT(func);
+    engine.fixed_update_func = func;
+}
+
+void engine_set_update_func(EngineUpdateFunc func)
+{
+    ASSERT_VALID_OBJECT(func);
+    engine.update_func = func;
+}
+
+void engine_set_render_func(EngineRenderFunc func)
+{
+    ASSERT_VALID_OBJECT(func);
+    engine.render_func = func;
+}
+
+double fixed_update_delta(void)
+{
+    return 1.0 / engine.settings.physics.fps;
+}
+
+void initialize_engine(void)
+{
+    sdl2_initialize();
+
+    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_INFO);
+
+    engine.running = setup_window() && setup_renderer();
+
+    engine.init_func();
+}
+
+bool setup_window(void)
+{
+    engine.window = sdl2_create_window(
+        engine.settings.window.title,
+        engine.settings.window.width, engine.settings.window.height
+    );
+    if (engine.window == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_VIDEO,
+            "Couldn't create SDL Window\n\tError: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+bool setup_renderer(void)
+{
+    Uint32 flags = SDL_RENDERER_ACCELERATED;
+    if (engine.settings.renderer.vsync) {
+        flags |= SDL_RENDERER_PRESENTVSYNC;
+    }
+
+    engine.renderer = sdl2_create_renderer_with_flags(engine.window, flags);
+    if (engine.renderer == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_VIDEO,
+            "Couldn't create SDL Renderer\n\tError: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+void finalize_engine(void)
+{
+    engine.quit_func();
+
+    destroy_renderer();
+    destroy_window();
+
+    sdl2_finalize();
+}
+
+void destroy_renderer(void)
+{
+    sdl2_destroy_renderer(engine.renderer);
+    engine.renderer = NULL;
+}
+
+void destroy_window(void)
+{
+    sdl2_destroy_window(engine.window);
+    engine.window = NULL;
+}
+
+void process_events(void)
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        engine.process_events_func(&event);
+    }
+}
+
+void fixed_update(void)
+{
+    if (engine.physics_tick_count <= fixed_update_delta()) {
+        return;
+    }
+
+    engine.fixed_update_func(fixed_update_delta());
+
+    engine.physics_tick_count -= fixed_update_delta();
+}
+
+void update(double delta)
+{
+    engine.update_func(delta);
+}
+
+void render(void)
+{
+    SDL_Color color = engine.settings.renderer.clear_color;
+    SDL_SetRenderDrawColor(engine.renderer, color.r, color.g, color.b, color.a);
+    SDL_RenderClear(engine.renderer);
+
+    engine.render_func();
+
+    SDL_RenderPresent(engine.renderer);
+}
---
+++ sdl2/engine.h
@@ -0,0 +1,54 @@
+#ifndef CSB_SDL2_ENGINE_H
+#define CSB_SDL2_ENGINE_H
+
+#include <sdl2/sdl2.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (*EngineInitFunc)(void);
+typedef void (*EngineQuitFunc)(void);
+typedef void (*EngineProcessEvents)(SDL_Event* event);
+typedef void (*EngineFixedUpdateFunc)(double delta);
+typedef void (*EngineUpdateFunc)(double delta);
+typedef void (*EngineRenderFunc)(void);
+
+typedef struct EngineSettings {
+    struct {
+        const char* title;
+        int width;
+        int height;
+    } window;
+    struct {
+        SDL_Color clear_color;
+        bool vsync;
+    } renderer;
+    struct {
+        double fps;
+    } physics;
+} EngineSettings;
+
+typedef struct Engine Engine;
+
+
+int engine_main_loop(EngineSettings settings);
+void engine_quit_loop(void);
+
+EngineSettings engine_default_settings(void);
+EngineSettings engine_settings(void);
+SDL_Window* engine_window(void);
+SDL_Renderer* engine_renderer(void);
+
+void engine_set_init_func(EngineInitFunc func);
+void engine_set_quit_func(EngineQuitFunc func);
+void engine_set_process_events_func(EngineProcessEvents func);
+void engine_set_fixed_update_func(EngineFixedUpdateFunc func);
+void engine_set_update_func(EngineUpdateFunc func);
+void engine_set_render_func(EngineRenderFunc func);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CSB_SDL2_ENGINE_H */
