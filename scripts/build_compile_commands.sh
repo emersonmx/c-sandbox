f6e5e78d3069454befb0bed59818786115c0a25b
Emerson MX <emersonmx@gmail.com>
2019-03-11 21:25:48 -0300

    Create symbolic link

--- scripts/build_compile_commands.sh
+++ scripts/build_compile_commands.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BUILD_DIR="$SCRIPT_DIR/../build"
+
 scons -Qc
 bear scons -j20
+
+pushd $BUILD_DIR
+ln -sf ../compile_commands.json
+popd
