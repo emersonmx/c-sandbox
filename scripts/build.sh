9ed7b1691ab89ceab29149041d40884e2d6534d1
Emerson MX <emersonmx@gmail.com>
2019-03-26 22:31:42 -0300

    Add script to build project

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+project_path="$script_dir/.."
+build_path="$project_path/build"
+
+if [[ ! -d "$build_path" ]]; then
+    CC=clang CXX=clang++ cmake -S "$project_path" -B "$build_path"
+fi
+
+make -C "$build_path" -j
