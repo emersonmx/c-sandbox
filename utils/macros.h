3b7fe7fb05563548b8a149caf138bdd342f48757
Emerson MX <emersonmx@gmail.com>
2019-03-31 04:48:56 -0300

    Remove IS_NULL macros

--- utils/macros.h
+++ utils/macros.h
@@ -3,15 +3,12 @@
 
 #include <assert.h>
 
-#define IS_NULL(X) X == NULL
-#define IS_NOT_NULL(X) X != NULL
-
 #define RETURN_VALUE_IF_TRUE(X, R) \
     if (X) {                       \
         return R;                  \
     }
 
-#define RETURN_VALUE_IF_NULL(X, R) RETURN_VALUE_IF_TRUE(IS_NULL(X), R)
+#define RETURN_VALUE_IF_NULL(X, R) RETURN_VALUE_IF_TRUE(!(X), R)
 #define RETURN_NULL_IF_NULL(X) RETURN_VALUE_IF_NULL(X, NULL)
 #define RETURN_IF_NULL(X) RETURN_VALUE_IF_NULL(X,)
 #define ASSERT_VALID_OBJECT(X) assert(X != NULL)
