adad5f4af09be560ca1fdae09dfcaf9c80b84450
Emerson MX <emersonmx@gmail.com>
2019-05-04 18:26:50 -0300

    Add shallow_clone function

--- tools/download_deps.sh
+++ tools/download_deps.sh
@@ -3,6 +3,11 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 EXTERNALDIR="$SCRIPT_DIR/../external"
 
+function shallow_clone()
+{
+    git clone --depth=1 --shallow-submodules $1
+}
+
 cd $EXTERNALDIR
 rm -rf cglm
-git clone https://github.com/recp/cglm.git
+shallow_clone https://github.com/recp/cglm.git
